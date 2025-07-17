5855f0135793a1c6db4cf5858738a8205e92dac1
Michel Oliveira <michel.nyx@hotmail.com>
2025-07-17 20:49:45 -0300

    chore: Add HTTP client support and improve database handling
    Updated `Program.cs` to configure HTTP clients for "default" and "fallback" processors. Modified payment handling to use `IHttpClientFactory` and added an `ON CONFLICT` clause to the SQL insert statement. Enhanced PostgreSQL connection string with pooling and timeout settings. Updated `docker-compose.yml` for service resource limits and modified `nginx.conf` for better connection handling.

--- src/backend/Program.cs
+++ src/backend/Program.cs
@@ -1,10 +1,14 @@
 using Dapper;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
 using System.Data;
 using System.Text.Json.Serialization;
 [module: DapperAot]
 
+
+const string defaultProcessorName = "default";
+const string fallbackProcessorName = "fallback";
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
@@ -13,38 +17,72 @@ builder.Services.ConfigureHttpJsonOptions(options =>
     options.SerializerOptions.TypeInfoResolverChain.Insert(0, JsonContext.Default);
 });
 
+builder.Services.AddHttpClient(defaultProcessorName, o =>
+    o.BaseAddress = new Uri(builder.Configuration.GetConnectionString(defaultProcessorName)!));
+
+builder.Services.AddHttpClient(fallbackProcessorName, o =>
+    o.BaseAddress = new Uri(builder.Configuration.GetConnectionString(fallbackProcessorName)!));
+
 builder.Services.AddTransient<IDbConnection>(sp =>
     new NpgsqlConnection(builder.Configuration.GetConnectionString("postgres")));
+
+if (builder.Environment.IsProduction())
+{
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(LogLevel.Error);
+}
 
 var app = builder.Build();
 
 var apiGroup = app.MapGroup("/");
 apiGroup.MapGet("/", () => Results.Ok());
 
-
-apiGroup.MapPost("payments", async ([FromBody] PaymentRequest request, IDbConnection conn) =>
+apiGroup.MapPost("payments", async ([FromBody] PaymentRequest request, IDbConnection conn, IHttpClientFactory factory) =>
 {
+    var httpDefault = factory.CreateClient(defaultProcessorName);
+
+    var requestedAt = DateTimeOffset.UtcNow;
+    try
+    {
+        var response = await httpDefault.PostAsJsonAsync("/payments", new ProcessorPaymentRequest
+        (
+            request.Amount,
+            requestedAt,
+            request.CorrelationId
+        ), JsonContext.Default.ProcessorPaymentRequest);
+
+        response.EnsureSuccessStatusCode();
+    }
+    catch
+    {
+        return Results.Conflict();
+    }
+
     var sql = @"
         INSERT INTO payments (correlation_id, processor, amount, requested_at)
-        VALUES (@CorrelationId, @Processor, @Amount, @RequestedAt);
+        VALUES (@CorrelationId, @Processor, @Amount, @RequestedAt)
+        ON CONFLICT (correlation_id) DO NOTHING;
     ";
 
     var parameters = new PaymentInsertParameters(
         CorrelationId: request.CorrelationId,
-        Processor: "default",
+        Processor: defaultProcessorName,
         Amount: request.Amount,
         RequestedAt: DateTime.UtcNow
     );
 
-    await conn.ExecuteAsync(sql, parameters);
+    int affectedRows = await conn.ExecuteAsync(sql, parameters);
+
+    if (affectedRows == 0)
+    {
+        return Results.Conflict();
+    }
 
     return Results.Accepted();
 });
 
 apiGroup.MapGet("/payments-summary", async ([FromQuery] DateTimeOffset? from, [FromQuery] DateTimeOffset? to, IDbConnection conn) =>
 {
-    const string defaultProcessorName = "default";
-    const string fallbackProcessorName = "fallback";
     const string sql = @"
         SELECT processor,
                COUNT(*) AS total_requests,
@@ -77,6 +115,11 @@ apiGroup.MapPost("/purge-payments", async (IDbConnection conn) =>
 
 app.Run();
 
+public sealed record ProcessorPaymentRequest(
+    decimal Amount,
+    DateTimeOffset RequestedAt,
+    Guid CorrelationId
+);
 public record PaymentInsertParameters(
     Guid CorrelationId,
     string Processor,
@@ -104,6 +147,7 @@ public record PaymentRequest(
     decimal Amount
 );
 
+[JsonSerializable(typeof(ProcessorPaymentRequest))]
 [JsonSerializable(typeof(PaymentRequest))]
 [JsonSerializable(typeof(PaymentSummaryResponse))]
 internal partial class JsonContext : JsonSerializerContext
